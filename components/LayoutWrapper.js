b59afe670b6bd6c5991f3954a079b4386461fbaf
derrickappah <derrickappah17@gmail.com>
2026-01-20 22:42:03 +0000

    style: final fine-tuning of bottom navigation bar height (2% increase)

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -38,7 +38,7 @@ export default function LayoutWrapper({ children, user }) {
     return (
         <>
             {!isEditingPage && <Navbar user={user} />}
-            <main className={isProductPage ? "" : (isEditingPage ? "" : "pt-16 pb-[64px]")}>
+            <main className={isProductPage ? "" : (isEditingPage ? "" : "pt-16 pb-[66px]")}>
                 {children}
             </main>
             {!isEditingPage && <MobileBottomNav user={user} />}
