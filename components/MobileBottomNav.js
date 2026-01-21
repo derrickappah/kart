d3f7b8564be1b1d382f20924f2469ebad3a3f43f
derrickappah <derrickappah17@gmail.com>
2026-01-21 16:13:48 +0000

    UI: Increase bottom navbar height by 5%

--- components/MobileBottomNav.js
+++ components/MobileBottomNav.js
@@ -37,7 +37,7 @@ export default function MobileBottomNav({ user }) {
     };
 
     return (
-        <nav className="fixed bottom-0 left-0 right-0 z-50 flex w-full justify-center border-t border-gray-100 bg-white/95 pb-[14px] pt-1 backdrop-blur-lg dark:border-gray-800 dark:bg-[#242428]/95">
+        <nav className="fixed bottom-0 left-0 right-0 z-50 flex w-full justify-center border-t border-gray-100 bg-white/95 pb-[17px] pt-1 backdrop-blur-lg dark:border-gray-800 dark:bg-[#242428]/95">
             <div className="flex w-full max-w-md items-center justify-between px-8">
                 {/* Home */}
                 <Link href="/" className="group flex flex-col items-center">
