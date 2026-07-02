2aa9aa6e58365e3222598926c75847c14fb4c04b
derrickappah <derrickappah17@gmail.com>
2026-07-02 10:30:14 +0000

    match messages page background to home page (white / dark:#242428)

--- app/dashboard/messages/layout.js
+++ app/dashboard/messages/layout.js
@@ -6,8 +6,8 @@ export default function MessagesLayout({ children }) {
     const isMessageDetails = pathname?.startsWith('/dashboard/messages/') && pathname !== '/dashboard/messages';
 
     return (
-        <div className={`bg-[#f6f7f8] dark:bg-[#111d21] w-full ${isMessageDetails ? 'h-[100dvh]' : 'h-[calc(100dvh-130px)] sm:h-[calc(100vh-130px)]'}`}>
-            <div className="relative flex flex-col h-full w-full max-w-md mx-auto bg-[#f6f7f8] dark:bg-[#111d21] overflow-hidden">
+        <div className={`bg-white dark:bg-[#242428] w-full ${isMessageDetails ? 'h-[100dvh]' : 'h-[calc(100dvh-130px)] sm:h-[calc(100vh-130px)]'}`}>
+            <div className="relative flex flex-col h-full w-full max-w-md mx-auto bg-white dark:bg-[#242428] overflow-hidden">
                 {children}
             </div>
         </div>
